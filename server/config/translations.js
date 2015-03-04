2bdad147b260400aecc3684e494f884b5f71a6bf
Josh <camman3d@gmail.com>
2015-03-03 21:56:32 -0500

    Server translations

---
+++ server/config/translations.js
@@ -0,0 +1,21 @@
+module.exports = {
+  en: {
+    'service-unauthorized': 'Unauthorized. Not your service.',
+    'no-user-created': 'No user created.',
+    'access-granted': 'Access granted and email sent.',
+    'no-open-registration': 'RoadAmico isn\'t allowing open registration yet.',
+    'invalid-card-data': 'Invalid card data.',
+    'no-user-found': 'No user found.',
+    'email-not-blank': 'Email cannot be blank',
+    'password-not-blank': 'Password cannot be blank',
+    'email-in-use': 'The specified email address is already in use.',
+    'invalid-password': 'Invalid password',
+
+    'test': 'Hello world'
+  },
+  fr: {
+    'service-unauthorized': 'Non autorisée. Ce n\'nest pas votre service.',
+
+    test: 'Bonjour monde'
+  }
+};

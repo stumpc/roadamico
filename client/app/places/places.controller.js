23a74a3fd6d78d92f36ad6b7d006df8b93adf9ea
haider_mahmood <haider.mahmood@gmail.com>
2015-05-23 02:36:09 +0500

    Delete a place being a user

--- client/app/places/places.controller.js
+++ client/app/places/places.controller.js
@@ -3,12 +3,24 @@
 angular.module('roadAmicoApp')
   .controller('PlacesCtrl', function ($scope, Place, Auth, placeUtil) {
     $scope.places = Place.query();
+    $scope.showLoader = false;
+
     $scope.places.$promise.then(function () {
       _.forEach($scope.places, function (result) {
         result.rating = placeUtil.getRating(result);
         result.photo = placeUtil.getPhoto(result);
       });
     });
+
+    $scope.deletePlace = function(place){
+        $scope.showLoader = true;
+        var params = {id: place._id};
+        Place.remove(params, function(){
+            $scope.places = Place.query();
+            $scope.showLoader = false;
+        });
+
+    };
 
     $scope.isLoggedIn = Auth.isLoggedIn;
   });

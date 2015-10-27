bcc08bd6cb317aac5db03e1040692b63b1ddbc74
stumpc <cstump@email.unc.edu>
2015-10-27 18:47:56 -0400

    changing after_success.sh for dev branch

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -4,14 +4,6 @@ echo "Host heroku.com" >> ~/.ssh/config
 echo "   StrictHostKeyChecking no" >> ~/.ssh/config
 echo "   CheckHostIP no" >> ~/.ssh/config;
 echo "   UserKnownHostsFile=/dev/null" >> ~/.ssh/config;
-if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]
-  then
-    gem install heroku
-    echo yes | heroku keys:add
-    grunt build
-    echo yes | grunt buildcontrol:heroku
-    heroku keys:remove `cat ~/.ssh/id_rsa.pub | cut -d \  -f 3`
-fi
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "development" ]]
   then
     gem install heroku

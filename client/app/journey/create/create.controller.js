fd239a4daa5da93e2ada1b2ff9246038cd426dd2
stumpc <cstump@email.unc.edu>
2015-10-29 22:42:27 -0400

    made sidebar in journey create use ui-sortable angularjs, needs style work

--- client/app/journey/create/create.controller.js
+++ client/app/journey/create/create.controller.js
@@ -3,4 +3,5 @@
 angular.module('roadAmicoApp')
   .controller('CreateCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.list = ["Old Well", "Clock Tower", "Kenan Stadium", "Davis Library", "Undergrad Library", "Franklin Street", "Polk Place"];
   });

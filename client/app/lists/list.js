46a27f6a8252163e5f84a1023163c2c529b152a9
wesleyabbey <wesleyabbey.io@gmail.com>
2015-11-10 23:41:55 -0500

    Added an edit field and cleaned up some code

--- client/app/lists/list.js
+++ client/app/lists/list.js
@@ -1,25 +1,22 @@
 'use strict';
 
 angular.module('roadAmicoApp')
-  .config(function ($stateProvider) {
+  .config(function($stateProvider) {
     $stateProvider
       .state('list', {
         url: '/lists/:id',
         template: '<ui-view autoscroll="true"></ui-view>',
         abstract: true,
         resolve: {
-          list: function ($stateParams, $q, List, Auth) {
+          list: function($stateParams, $q, List, Auth) {
             var deferred = $q.defer();
-            Auth.isLoggedInAsync(function (isLoggedIn) {
-              if (isLoggedIn) {
-                List.get({id: $stateParams.id}).$promise.then(function (list) {
-                  deferred.resolve(list);
-                });
-              } else {
-                List.getPublic({id: $stateParams.id}).$promise.then(function (list) {
-                  deferred.resolve(list);
-                });
-              }
+            Auth.isLoggedInAsync(function(isLoggedIn) {
+
+              List.get({
+                id: $stateParams.id
+              }).$promise.then(function(list) {
+                deferred.resolve(list);
+              });
             });
             return deferred.promise;
           }

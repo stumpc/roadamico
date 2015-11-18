7d93590b2305437405ff4fe47627a5f54dcedfc6
stumpc <cstump@email.unc.edu>
2015-11-17 23:45:19 -0500

    Adding functionality for side-bar items to be clickable

--- client/app/journey/create/create.controller.js
+++ client/app/journey/create/create.controller.js
@@ -3,5 +3,17 @@
 angular.module('roadAmicoApp')
   .controller('CreateCtrl', function ($scope) {
     $scope.message = 'Hello';
-    $scope.list = ["Old Well", "Clock Tower", "Kenan Stadium", "Davis Library", "Undergrad Library", "Franklin Street", "Polk Place", "Wesley's House"];
+
+    var x = {
+    	"Old Well":"OldWell",
+    	"Clock Tower":"ClockTower",
+    	"Kenan Stadium":"KenanStadium",
+    	"Davis Library":"DavisLibrary",
+    	"Undergrad Library":"UndergradLibrary",
+    	"Franklin Street":"FranklinStreet",
+    	"Polk Place":"PolkPlace",
+    	"Wesley's House":"WesleysHouse"
+    };
+    //$scope.list = ["Old Well", "Clock Tower", "Kenan Stadium", "Davis Library", "Undergrad Library", "Franklin Street", "Polk Place", "Wesley's House"];
+    $scope.list = x;
   });
